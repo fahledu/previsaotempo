1b9c8520c10fbd7a3667b9756e7246d1bba759c5
fahledu <fahledu@gmail.com>
2023-06-03 18:37:05 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,23 @@
+
+# Previsão do tempo
+
+Acesse dados climáticos atualizados para qualquer localidade na Terra. Saiba as condições meteorológicas em tempo real, incluindo temperatura, umidade, descrição do tempo e muito mais. Com essa ferramenta, você pode obter informações precisas sobre o clima em qualquer cidade do mundo. Esteja você planejando uma viagem, precisando se vestir adequadamente ou simplesmente curioso sobre as condições climáticas em outras regiões, nossa plataforma oferece acesso fácil e rápido aos dados climáticos confiáveis. Fique por dentro das previsões meteorológicas e desfrute de uma experiência personalizada com informações climáticas em sua localização atual ou em qualquer lugar que desejar explorar.
+
+### Funcionalidades
+
+- Reconhecimento automatico da geolocalização do navegador.
+- Caso o usuáro negue acesso a localização, é atribuido por default os dados do clima de São Paulo.
+- Ou busca por qualquer outra região através do campo pesquisa.
+
+### Screenshot
+
+![Mobile](screenshot/mobile.PNG)
+![PC](screenshot/PC.PNG)
+![errorLocation](screenshot/errorLocation.PNG)
+![serch](screenshot/search.PNG)
+
+### Detalhes Tecnicos
+
+- Para utilizar esse script é necessário criar uma conta no site https://openweathermap.org/ e criar uma API KEY e no arquivo script.js coloca-la em KEY
+
+![apikey](screenshot/apikey.PNG)
---
+++ script.js
@@ -0,0 +1,56 @@
+//Criar uma conta no https://openweathermap.org/ criar uma API KEY e substituir abaixo
+const key = "3d057137d9125babe4e586d8a03do8d912"; 
+
+document.addEventListener('DOMContentLoaded', () => {
+    const updateWeatherData = (data) => {
+        document.querySelector('.city').innerHTML = `Tempo em ${data.name}`;
+        document.querySelector('.temp').innerHTML = `${Math.floor(data.main.temp)}ºC`;
+        document.querySelector('.img-weather').src = `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`;
+        document.querySelector('.text-weather').innerHTML = `${data.weather[0].description}`;
+        document.querySelector('.humidity').innerHTML = `Umidade: ${data.main.humidity}%`;
+    };
+
+    const getLocation = async () => {
+        try {
+            const position = await new Promise((resolve, reject) => {
+                navigator.geolocation.getCurrentPosition(resolve, reject);
+            });
+            const { latitude: lat, longitude: lon } = position.coords;
+
+            const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${key}`);
+            const data = await response.json();
+            const city = data.name;
+
+            const weatherResponse = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city},BRA&appid=${key}&lang=pt_br&units=metric`);
+            const weatherData = await weatherResponse.json();
+
+            updateWeatherData(weatherData);
+        } catch (error) {
+            console.log('Erro ao obter localização', error);
+            
+            const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=sao%20paulo,BRA&appid=${key}&lang=pt_br&units=metric`);
+            const data = await response.json();
+
+            updateWeatherData(data);
+        }
+    };
+
+    getLocation();
+
+    const searchCity = async (input) => {
+        try {
+            const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${input},BRA&appid=${key}&lang=pt_br&units=metric`);
+            const data = await response.json();
+
+            updateWeatherData(data);
+        } catch (error) {
+            console.log('Erro ao obter dados do clima', error);
+        }
+    };
+
+    const button = document.querySelector('button');
+    button.addEventListener('click', () => {
+        const input = document.querySelector('.input').value;
+        searchCity(input);
+    });
+});
